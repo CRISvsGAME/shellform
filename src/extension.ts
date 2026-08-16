db1ea46e03788d7b68b4d3a70ae20bf9d09c6d55
CRISvsGAME <cris@crisvsgame.com>
2026-08-16 20:00:00 +0000

    Shellform 0.0.9
    Spawn Shell Formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { spawn } from "child_process";
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext): void {
@@ -9,15 +10,44 @@ export function activate(context: vscode.ExtensionContext): void {
             provideDocumentFormattingEdits(
                 document: vscode.TextDocument,
                 options: vscode.FormattingOptions,
-            ): vscode.TextEdit[] {
-                console.log("Shellform Formatter Invoked");
-                console.log(document.getText());
-
+            ): Promise<vscode.TextEdit[]> {
                 const indent = options.insertSpaces ? options.tabSize : 0;
 
-                console.log({ insertSpaces: options.insertSpaces, tabSize: options.tabSize, indent });
+                return new Promise((resolve) => {
+                    const process = spawn("shfmt", ["-i", indent.toString()]);
 
-                return [];
+                    let stdout = "";
+                    let stderr = "";
+
+                    process.stdout.setEncoding("utf8");
+                    process.stderr.setEncoding("utf8");
+
+                    process.stdout.on("data", (data: string) => {
+                        stdout += data;
+                    });
+
+                    process.stderr.on("data", (data: string) => {
+                        stderr += data;
+                    });
+
+                    process.on("error", (error) => {
+                        console.error(error);
+                        resolve([]);
+                    });
+
+                    process.on("close", (code: number) => {
+                        if (code !== 0) {
+                            console.error(stderr);
+                            resolve([]);
+                            return;
+                        }
+
+                        console.log(stdout);
+                        resolve([]);
+                    });
+
+                    process.stdin.end(document.getText());
+                });
             },
         },
     );
